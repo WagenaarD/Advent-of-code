a29251b2de07042c939ba3ce106a47f5b4863072
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2023-12-12 07:52:46 +0100

    12 p2

--- 2023/day_12/main.py
+++ 2023/day_12/main.py
@@ -3,101 +3,61 @@ Advent of code challenge 2023
 python3 ../../aoc_tools/get_aoc_in.py
 python3 main.py < in
 """
+# 06:00:00
 # 06:35:18
+# 07:50:00
 
-AOC_ANSWER = (None, None)
+AOC_ANSWER = (7286, 25470469710341)
 
 import sys
 sys.path.append('../..')
-from aoc_tools import *
-import itertools as it
-from dataclasses import dataclass, field
-from collections import defaultdict
+from aoc_tools import print_function
 import re
-import numpy as np
-from pprint import pprint
 from functools import cache
-import math
-
-def blue_print_versions(bprint: str) -> 'list[str]':
-    mtch = re.search('\?', bprint)
-    idx = mtch.start()
-    out = [bprint[:idx] + char + bprint[idx+1:] for char in '.#']
-    if not '?' in out[0]:
-        return out
-    else:
-        return blue_print_versions(out[0]) + blue_print_versions(out[1])
-
-#     return [bprint[:idx] + char + bprint[idx+1:] for char in '.#']
-
-# def get_all_combos(bprint: str) -> 'list[str]':
-#     combos = replace_one(bprint)
-#     for combo in combos:
-#         if not '?' in combo:
-#             continue
-#         combos.remove(combo)
-#         combos += replace_one(combo)
-#     return combos
 
 
-@print_function()
-def main(input: str) -> int:
-    lines = input.split('\n')
+@cache
+def possible_solutions(bprint: str, score: 'tuple[int]') -> int:
+    # Pruning
+    if sum(score) + len(score) + 1 > len(bprint):
+        return 0
+    # Scoring
+    if not score:
+        if bprint.count('#') == 0:
+            return 1
+        else:
+            return 0
+    # Branching
+    val = score[0]
+    new_score = score[1:]
     ans = 0
-    # .??..??...?##. 1,1,3
-    for line_idx, line in enumerate(lines):
-        print(line_idx, len(lines))
-        # print('')
-        # print(line)
-        
-        
-        bprint, vals = line.split()
-        score = [int(num) for num in vals.split(',')]
-        # print(vals)
-        for new_bprint in blue_print_versions(bprint):
-            new_score = [len(mtch.group()) for mtch in re.finditer('\#+', new_bprint)]
-            # print(new_bprint, new_score, score == new_score)
-            if score == new_score:
-                ans += 1
-
-
-
-        # 
-        # for iter_vals in it.permutations('#.', bprint.count('?')):
-        # for iter_vals in it.combinations_with_replacement('#.', bprint.count('?')):
-            
-        #     new_line = bprint
-        #     for mtch_idx, mtch in enumerate(re.finditer('\?', new_line)):
-        #         char = iter_vals[mtch_idx]
-        #         idx = mtch.start()
-        #         new_line = new_line[:idx] + char + new_line[idx+1:]
-        #         # print('', new_line)
-        #     print(new_line, iter_vals)
-
-
-            # for char in '.#':
-            #     new_line = bprint[:idx-1] + char + bprint[idx:]
-                
-            #     cur_score = []
-            #     while '?' in new_line:
-            #         cur_mtch = re.search('\?+', new_line)
-            #         cur_score.append(len(cur_mtch.group()))
-            #         new_line = new_line[cur_mtch.start()+1:]
-            #     if cur_score == score:
-            #         ans += 1
-        # break
+    match_string = '(?=([.?][#?]{' + str(val) + '}[.?]))'
+    for mtch in re.finditer(match_string, bprint):
+        if not mtch:
+            continue
+        if '#' in bprint and mtch.start()+1 > bprint.find('#'):
+            continue
+        end = mtch.start() + val
+        new_bprint = bprint[end+2:] 
+        ans += possible_solutions('.' + new_bprint, new_score)
     return ans
 
 
+@print_function()
+def solve(input: str, factor: int = 5) -> int:
+    lines = input.split('\n')
+    ans = 0
+    for line in lines:
+        bprint, vals = line.split()
+        bprint = '?'.join([bprint] * factor)
+        vals = ','.join([vals] * factor)
+        score = tuple(int(num) for num in vals.split(','))
+        ans += possible_solutions('.' + bprint + '.', score)
+    return ans
 
 
-
-
-
-
-
-
-
+def main(input: str) -> 'tuple(int, int)':
+    return (solve(input, 1), solve(input, 5))
 
 
 if __name__ == '__main__':
