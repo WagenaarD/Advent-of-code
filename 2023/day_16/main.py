6efe071a96c157ab9bffad1947b91afe710784ba
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2023-12-16 14:29:22 +0100

    d16p2 finished

--- 2023/day_16/main.py
+++ 2023/day_16/main.py
@@ -6,21 +6,13 @@ python3 main.py < in
 # Start, Part 1, Part 2
 # 13:52:06
 # 14:19:59
-# 
+# 14:25:58
 
-AOC_ANSWER = (None, None)
+AOC_ANSWER = (7860, 8331)
 
 import sys
 sys.path.append('../..')
-from aoc_tools import *
-import itertools as it
-from dataclasses import dataclass, field
-from collections import defaultdict
-import re
-import numpy as np
-from pprint import pprint
-from functools import cache
-import math
+from aoc_tools import print_function
 
 DIRS = {
     '>': ( 0,  1),
@@ -30,23 +22,19 @@ DIRS = {
 }
 
 
-@print_function()
-def main(input: str) -> int:
-    grid = input.split('\n')
+def solve(grid, beam):
+    new_beams = [beam]
     seen_states = set()
-    new_beams = [(0,-1,'>')]
     while new_beams:
         beams = new_beams
         new_beams = []
         for r, c, dir in beams:
-            # new_beams = []
             dr, dc = DIRS[dir]
             rr, cc = r + dr, c + dc
             out_of_bounds = not (0 <= rr < len(grid) and 0 <= cc < len(grid[0]))
             if out_of_bounds:
                 continue
             char = grid[rr][cc]
-            print(rr, cc, char)
             if char == '.':
                 new_beams.append((rr, cc, dir))
             elif char == '|':
@@ -81,39 +69,39 @@ def main(input: str) -> int:
                     new_beams.append((rr, cc, '<'))
             else:
                 assert False, 'Unhandled char'
-        print('new_beams (before filtering)', new_beams)
         new_beams = [beam for beam in new_beams if not beam in seen_states]
-        print('new_beams (after filtering)', new_beams)
         for beam in new_beams:
             seen_states.add(beam)
     
-    print(seen_states)
-    seen_pos = [(r, c) for r, c, _ in seen_states]
-    for r, row in enumerate(grid):
-        print(''.join(['#' if (r,c) in seen_pos else '.' for c, _ in enumerate(row)]))
-    
-    return len(set(seen_pos))
-                
+    seen_pos = set([(r, c) for r, c, _ in seen_states])
+
+    return len(seen_pos)
 
 
-        
+@print_function()
+def part_one(grid: 'list[str]') -> int:
+    beam = (0,-1,'>')
+    return solve(grid, beam)
+
+
+@print_function()
+def part_two(grid: list[str]) -> int:
+    scores = []
+    for r in range(len(grid)):
+        scores.append(solve(grid, (r, -1, '>')))
+        scores.append(solve(grid, (r, len(grid[0]), '<')))
+    for c in range(len(grid[0])):
+        scores.append(solve(grid, (-1, c, 'v')))
+        scores.append(solve(grid, (len(grid), c, '^')))
+    return max(scores)
+
+
+def main(input: str) -> tuple[int, int]:
+    grid = input.split('\n')
+    return (part_one(grid), part_two(grid))
             
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     """Executed if file is executed but not if file is imported."""
     input = sys.stdin.read().strip()
     print('  ->', main(input) == (AOC_ANSWER[0], AOC_ANSWER[1]))
-
-
