750c10c751b0153e1e74e27949e19035c027e504
Dirk Wagnaar <d.wagenaar@umcg.nl>
2023-12-18 13:29:54 +0100

    2023 day_18 part two

--- 2023/day_18/main.py
+++ 2023/day_18/main.py
@@ -6,7 +6,7 @@ python3 main.py < in
 # Start, Part 1, Part 2
 # 12:22:53
 
-AOC_ANSWER = (None, None)
+AOC_ANSWER = (49897, 194033958221830)
 
 import sys
 sys.path.append(AOC_BASE_PATH := '/'.join(__file__.replace('\\', '/').split('/')[:-3]))
@@ -26,6 +26,12 @@ DIRS = {
     'R': ( 0,  1),
     'L': ( 0, -1),
 }
+HEX_DICT = {
+    '0': 'R',
+    '1': 'D',
+    '2': 'L',
+    '3': 'U',
+}
 
 def visualize_part_one(trench: set[tuple[int, int]], seen: set[tuple[int, int]] = set()) -> None:
     
@@ -41,7 +47,7 @@ def visualize_part_one(trench: set[tuple[int, int]], seen: set[tuple[int, int]] 
 
 
 @print_function()
-def part_one(input: str) -> int:
+def part_one(input: str, vis: bool = True) -> int:
     lines = input.split('\n')
     trench = set()
     pos = (0,0)
@@ -51,7 +57,7 @@ def part_one(input: str) -> int:
         for _ in range(int(dl)):
             pos = tuple(p + dp for p, dp in zip(pos, dr))
             trench.add(pos)
-    # visualize_part_one(trench)
+    if vis: visualize_part_one(trench)
     
     # Grow from outside
     r_min = min(r for r, c in trench) - 1
@@ -71,7 +77,8 @@ def part_one(input: str) -> int:
             if in_bounds and new_pos not in seen and new_pos not in trench:
                 seen.add(new_pos)
                 stack.add(new_pos)
-    # visualize_part_one(trench, seen)
+    
+    if vis: visualize_part_one(trench, seen)
     ans = 0
     for r in range(r_min, r_max + 1):
         for c in range(c_min, c_max + 1):
@@ -80,23 +87,46 @@ def part_one(input: str) -> int:
     return ans
 
 
-@print_function()
-def part_two(input: str) -> int:
+def get_area(nodes: list[tuple[int, int]]) -> int:
+    """
+    Polygon area: Signed area: https://www.mathopenref.com/coordpolygonarea.html
+    Correction of total_l is necessary as the algorithm calculates the area between a line running 
+    in the center of the pixel. However, we want to include the whole pixel:
+    - Half the line lengths is added since half of each straight piece is excluded in the signed area
+    - One pixel is added to account for outer four corners which have 3/4 excluded.
+    - For any additional turn, one corner will have 3/4 excluded and one 1/4 excluded, so no 
+    additional correction is needed since they are compensated with 1/2 per length.
+    """
+    area = 0
+    total_l = 0
+    for cur, nxt in zip(nodes, nodes[1:]):
+        total_l += abs(cur[0] - nxt[0]) + abs(cur[1] - nxt[1])
+        area += (cur[0] * nxt[1] - cur[1] * nxt[0]) / 2
+    return int(abs(area) + 1 + total_l / 2)
+
+
+def solve(input: str, part_two = False) -> int:
     lines = input.split('\n')
-
-
-
-
-
+    pos = (0,0)
+    nodes = [pos]
+    for line in lines:
+        dr, dl, color = line.split()
+        dr = DIRS[dr]
+        if part_two:
+            dr = DIRS[HEX_DICT[color[-2]]]
+            dl = int(color[2:-2], 16)
+        pos = tuple(p + dp * int(dl) for p, dp in zip(pos, dr))
+        nodes.append(pos)
+    return get_area(nodes)
     
-# @print_function()
-# def main(input: str) -> tuple[int, int]:
-#     return (part_one(input), part_two(input))
+@print_function()
+def main(input: str) -> tuple[int, int]:
+    return (solve(input, False), solve(input, True))
+
 if __name__ == '__main__':
     """Executed if file is executed but not if file is imported."""
     input = sys.stdin.read().strip()
-    print(part_one(input) == AOC_ANSWER[0])
-    print(part_two(input) == AOC_ANSWER[1])
-    # print('  ->', main(input) == (AOC_ANSWER[0], AOC_ANSWER[1]))
+    print(part_one(input, input.count('\n') < 100) == AOC_ANSWER[0])
+    print('  ->', main(input) == (AOC_ANSWER[0], AOC_ANSWER[1]))
 
 
