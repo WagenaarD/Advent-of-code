201dc03cbb46d4dfca42e3637689a5b9fdc7674f
Dirk Wagnaar <d.wagenaar@umcg.nl>
2023-12-18 09:56:22 +0100

    2023p17 dijkstra exercise

--- 2023/day_00/main.py
+++ 2023/day_00/main.py
@@ -8,7 +8,7 @@ python3 main.py < in
 AOC_ANSWER = (None, None)
 
 import sys
-sys.path.append('../..')
+sys.path.append(AOC_BASE_PATH := '/'.join(__file__.replace('\\', '/').split('/')[:-3]))
 from aoc_tools import print_function
 import itertools as it
 from dataclasses import dataclass, field
