ab9080cf2cdcab571073ccb5f9b10a44cac86a4f
Dirk Wagenaar <d.wagenaar@umcg.nl>
2024-12-03 06:35:15 +0100

    2024 day_03

--- 2024/day_02/main.py
+++ 2024/day_02/main.py
@@ -14,6 +14,9 @@ from aoc_tools import print_function, aoc_run
 
 
 def is_safe(report):
+    # # Tamara did this, which I think is more elegant
+    # report_differences = {n2-n1 for n1, n2 in zip(report, report[1:])}
+    # return report_differences <= {1, 2, 3} or report_differences <= {-1, -2, -3}
     signs, diffs = set(), []
     for n1, n2 in zip(report, report[1:]):
         diffs.append(abs(n2-n1))
@@ -31,9 +34,8 @@ def main(input: str) -> tuple[int, int]:
         report = list(map(int, line.split()))
         p1 += is_safe(report)
         for idx in range(len(report)):
-            new_line = report[:idx] + report[idx+1:]
-            if is_safe(new_line):
-                p2 +=1 
+            if is_safe(report[:idx] + report[idx+1:]):
+                p2 += 1
                 break
     return p1, p2
     
---
+++ 2024/day_03/main.py
@@ -0,0 +1,40 @@
+"""
+Advent of code challenge
+python3 ../../aoc_tools/get_aoc_in.py
+python3 main.py < in
+"""
+# Start, Part 1, Part 2
+
+AOC_ANSWER = (175700056, 71668682)
+
+import sys
+from pathlib import Path
+sys.path.append(str(AOC_BASE_PATH := Path(__file__).parents[2]))
+from aoc_tools import print_function, aoc_run
+import re
+
+
+@print_function
+def part_one(input: str) -> int:
+    ans = 0
+    for left, right in re.findall('mul\\((\\d+),(\\d+)\\)', input):
+        ans += int(left) * int(right)
+    return ans
+
+
+@print_function
+def part_two(input: str) -> int:
+    input = input.replace('\n', ' ')
+    lines = re.findall('do\\(\\)(.*?)don\'t\\(\\)', 'do()' + input + 'don\'t()')
+    return sum(part_one(line) for line in lines)
+
+    
+@print_function
+def main(input: str) -> tuple[int, int]:
+    return (
+        part_one(input), 
+        part_two(input)
+    )
+aoc_run(__name__, __file__, main, AOC_ANSWER, 'in')
+
+
