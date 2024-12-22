5a45e14e95fc58de9bfeb260ac8603d5745bbe60
Dirk Wagenaar <d.wagenaar@umcg.nl>
2024-12-22 15:19:07 +0100

    minor cleanups

--- 2024/day_21/main.py
+++ 2024/day_21/main.py
@@ -7,10 +7,17 @@ python3 main.py < in
 import sys
 from pathlib import Path
 sys.path.append(str(AOC_BASE_PATH := Path(__file__).parents[2]))
-from aoc_tools import print_function, aoc_run, tuple_sub
+from aoc_tools import print_function, aoc_run, tuple_sub, tuple_add
 from functools import cache
 
 AOC_ANSWER = (107934, 130470079151124)
+
+DIRS = {
+    '^': (-1, 0),
+    '>': (0, 1),
+    'v': (1, 0),
+    '<': (0, -1),
+}
 
 NUMPAD_POS = {
     '7': (0, 0),
@@ -34,66 +41,48 @@ DIRPAD_POS = {
     '>': (1, 2),
 }
 
+
 @cache
-def numeric_keypad(target: str, start: str = 'A') -> list[tuple[str]]:
+def generic_keypad(target: str, start: str, is_directional: bool) -> list[tuple[str]]:
     """
     Calculates all possible options (= a tuple of strings with each string equal to <, >, ^ or v) 
     which would result in a button press of target (= a string indicating the button to press) when
     the current position is start (= a string indicating the button previously pressed).
 
-    Position (row, column)
-          0   1   2
-        +---+---+---+
-      0 | 7 | 8 | 9 |
-        +---+---+---+
-      1 | 4 | 5 | 6 |
-        +---+---+---+
+    This function allows for two designs, the directional keypad (is_directional = True) and 
+    numerical keypad (=False). The only differences are the key to coordinate layour (dir_dict) and 
+    the coordinate through which travel is forbidden (forbidden).
+
+    Numerical keypad             Directional keypad
+          0   1   2                   0   1   2
+        +---+---+---+                   +---+---+
+      0 | 7 | 8 | 9 |             0     | ^ | A |
+        +---+---+---+               +---+---+---+
+      1 | 4 | 5 | 6 |             1 | < | v | > |
+        +---+---+---+               +---+---+---+
       2 | 1 | 2 | 3 |
         +---+---+---+
       3     | 0 | A |
             +---+---+
     """
-    target_pos = NUMPAD_POS[target]
-    start_pos = NUMPAD_POS[start]
-    dpos = tuple_sub(target_pos, start_pos)
+    dir_dict = DIRPAD_POS if is_directional else NUMPAD_POS
+    forbidden = (0, 0) if is_directional else (3, 0)
+    target_pos = dir_dict[target]
+    start_pos = dir_dict[start]
+    dpos = tuple_sub(target_pos, start_pos)    
     moves = ['v' for _ in range(dpos[0])] + ['^' for _ in range(-dpos[0])] \
         + ['>' for _ in range(dpos[1])] + ['<' for _ in range(-dpos[1])]
-    permutations = [tuple(moves) + ('A',), tuple(reversed(moves)) + ('A',)]
-    # Remove corner option when moving from bottom right to top left
-    if 3 == start_pos[0] and 0 == target_pos[1]:
-        permutations.remove(('<',) * -dpos[1] + ('^',) * -dpos[0] + ('A',))
-    # Remove corner option when moving from top left to bottom right
-    elif 3 == target_pos[0] and 0 == start_pos[1]:
-        permutations.remove(('v',) * dpos[0] + ('>',) * dpos[1] + ('A',))
-    return permutations
-
-@cache
-def directional_keypad(target: str, start: str = 'A') -> list[tuple[str]]:
-    """
-    Calculates all possible options (= a tuple of strings with each string equal to <, >, ^ or v) 
-    which would result in a button press of target (= a string indicating the button to press) when
-    the current position is start (= a string indicating the button previously pressed).
-
-    Position (row, column)
-          0   1   2
-            +---+---+
-      0     | ^ | A |
-        +---+---+---+
-      1 | < | v | > |
-        +---+---+---+
-    """
-    target_pos = DIRPAD_POS[target]
-    start_pos = DIRPAD_POS[start]
-    dpos = tuple_sub(target_pos, start_pos)
-    moves = ['v' for _ in range(dpos[0])] + ['^' for _ in range(-dpos[0])] \
-        + ['>' for _ in range(dpos[1])] + ['<' for _ in range(-dpos[1])]
-    permutations = [tuple(moves) + ('A',), tuple(reversed(moves)) + ('A',)]
-    if 0 == start_pos[0] and 0 == target_pos[1]:
-        # Remove corner option when moving from top right to bottom left
-        permutations.remove(('<',) * -dpos[1] + ('v',) * dpos[0] + ('A',))
-    elif 0 == target_pos[0] and 0 == start_pos[1]:
-        # Remove corner option when moving from top left to bottom right
-        permutations.remove(('^',) * -dpos[0] + ('>',) * dpos[1] + ('A',))
+    permutations = list({tuple(moves) + ('A',), tuple(reversed(moves)) + ('A',)})
+    # Remove paths travelling through the forbidden coordinate.
+    if all(forbidden[idx] in (start_pos[idx], target_pos[idx]) for idx in range(2)):
+        pos = start_pos
+        for move in moves:
+            pos = tuple_add(pos, DIRS[move])
+            if pos == forbidden:
+                permutations.pop(0)
+                break
+        else:
+            permutations.pop()
     return permutations
 
 
@@ -104,7 +93,7 @@ def process_direction_press(target: str, source: str, depth: int) -> int:
     robot of a connected set of {depth} robots. Is called recursively.
     """
     # Calculate in how many way we can press the target at depth-1
-    key_press_options = directional_keypad(target, source)
+    key_press_options = generic_keypad(target, source, True)
     if depth == 1:
         return min(len(row) for row in key_press_options)
     else:
@@ -129,8 +118,9 @@ def process_character(code, start = 'A', depth = 2):
     # Find the options for directional moves for the final robot
     key_press_options = [tuple()]
     for target in code:
-        new_key_presses = numeric_keypad(target, start)
-        key_press_options = [old_keys + new_keys for old_keys in key_press_options for new_keys in new_key_presses]
+        new_key_presses = generic_keypad(target, start, False)
+        key_press_options = [old_keys + new_keys for old_keys in key_press_options 
+                             for new_keys in new_key_presses]
         start = target
     # Calculate the number of steps required for each of these directional options
     key_press_lengths = []
--- 2024/day_22/main.py
+++ 2024/day_22/main.py
@@ -28,6 +28,23 @@ def next_secret(secret: int) -> int:
     secret %= 16777216
     return secret
 
+def encode_last_four(current: int, new: int) -> int:
+    """
+    Stores 5 numbers between -9 and +9 (inclusive) as a single int, taking 5 bits for each
+    
+    Currently not used, but when used instead of a tuple it made the code a factor ~7 faster when 
+    using pypy, but not so much when using python3
+    """
+    return ((current << 5) + (new + 9)) % (2**(5*4))
+
+def decode_last_four(last_four: int) -> list[int]:
+    """
+    Converts an int into a list of 5 numbers between -9 and +9 (inclusive). 5 bits for each
+    
+    Currently not used, but when used instead of a tuple it made the code a factor ~7 faster when 
+    using pypy, but not so much when using python3
+    """
+    return [(last_four//(2**(idx*5)))%(2**5)-9 for idx in reversed(range(4))]
 
 @print_function
 def main(input_txt: str) -> tuple[int, int]:
@@ -35,20 +52,18 @@ def main(input_txt: str) -> tuple[int, int]:
     For each initial number, calculate the next 2000 numbers. For p1, add the last number to the 
     score. For p2, keep track of the pattern, defined as the last four differences, and add the 
     sell-price for that pattern to a dictionary the first time it occurs.
+    last_four could be stored as 
     """
     initial_numbers = list(map(int, input_txt.split('\n')))
     p1 = 0
     p2_scores = defaultdict(int)
     for current in initial_numbers:
-        differences = []
         seen = set()
+        last_four = (0,)
         for idx in range(2000):
             previous, current = current, next_secret(current)
-            differences.append(current%10 - previous%10)
-            if idx < 4:
-                continue
-            last_four = tuple(differences[-4:])
-            if last_four not in seen:
+            last_four = last_four[-3:] + (current%10 - previous%10,)
+            if idx > 3 and last_four not in seen:
                 seen.add(last_four)
                 p2_scores[last_four] += current%10
         p1 += current
